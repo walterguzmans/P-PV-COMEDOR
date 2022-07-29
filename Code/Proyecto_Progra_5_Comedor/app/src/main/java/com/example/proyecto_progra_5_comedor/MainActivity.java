fc23f4a3cb70cce80c148fcda8dc237724cc1f60
Laiaz <cucuymr@gmail.com>
2022-07-29 17:12:26 -0600

    Fe Principal
    completado el FE principal

--- Code/Proyecto_Progra_5_Comedor/app/src/main/java/com/example/proyecto_progra_5_comedor/MainActivity.java
+++ Code/Proyecto_Progra_5_Comedor/app/src/main/java/com/example/proyecto_progra_5_comedor/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.proyecto_progra_5_comedor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnLogin;
     EditText txtUsuario;
     EditText txtContraseña;
 
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.btnLogin);
         txtUsuario = findViewById(R.id.txtUsuario);
         txtContraseña = findViewById(R.id.txtContraseña);
     }
@@ -30,12 +34,16 @@ public class MainActivity extends AppCompatActivity {
         String contra = txtContraseña.getText().toString();
 
         if(usu.equals(Utilidades.NOMBRE_USUARIO) && contra.equals(Utilidades.CONTRASEÑA_USUARIO)){
+            Intent intent = new Intent(MainActivity.this,MenuEstudiante.class);
+            startActivity(intent);
             Toast.makeText(this, "Logueo exitoso de Estudiante", Toast.LENGTH_SHORT).show();
-            //ALVARO SIGA DESDE AQUI, REDIRIGA LA OSTIA
+
         }
         else if (usu.equals(Utilidades.NOMBRE_ADMIN) && contra.equals(Utilidades.CONTRASEÑA_ADMIN)){
+            Intent intent = new Intent(MainActivity.this,MenuAdmin.class);
+            startActivity(intent);
             Toast.makeText(this, "Logueo exitoso de Admin", Toast.LENGTH_SHORT).show();
-            //ALVARO SIGA DESDE AQUI, REDIRIGA LA OSTIA
+
         }
         else{
             Toast.makeText(this, "Logueo fallido", Toast.LENGTH_SHORT).show();
