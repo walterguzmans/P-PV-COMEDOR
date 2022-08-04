fdb0aa1a901534e674573672480488c13769d0fc
Laiaz <cucuymr@gmail.com>
2022-08-04 15:33:43 -0600

    Bade de datos lista
    se modifico el archivo para que la base de datos trabajara con sqlite

--- Code/Proyecto_Progra_5_Comedor/app/src/main/java/com/example/proyecto_progra_5_comedor/ConexionDB.java
+++ Code/Proyecto_Progra_5_Comedor/app/src/main/java/com/example/proyecto_progra_5_comedor/ConexionDB.java
@@ -5,47 +5,28 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.Nullable;
+
 public class ConexionDB extends SQLiteOpenHelper {
-    private static final String NOMBRE_BD = "COMEDOR_BD";
-    private static final int VERSION_BD = 1;
 
-    public ConexionDB(Context context) {
-        super(context, "COMEDOR_BD", null, VERSION_BD);
+
+    public ConexionDB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db){
-        db.execSQL("CREATE TABLE " + Utilidades.TABLA_USUARIO + " ("
-                + Utilidades.ID_USUARIO + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + Utilidades.NOMBRE_USUARIO + " TEXT NOT NULL,"
-                + Utilidades.CONTRASEÑA_USUARIO + " TEXT NOT NULL,"
-                + "UNIQUE ("+ Utilidades.ID_USUARIO + "))");
-
-        db.execSQL("CREATE TABLE " + Utilidades.TABLA_ADMIN + " ("
-                + Utilidades.ID_ADMIN + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + Utilidades.NOMBRE_ADMIN + " TEXT NOT NULL,"
-                + Utilidades.CONTRASEÑA_ADMIN + " TEXT NOT NULL,"
-                + "UNIQUE ("+ Utilidades.ID_ADMIN + "))");
-
-
-        ContentValues values = new ContentValues();
-
-        values.put(Utilidades.ID_USUARIO, "100");
-        values.put(Utilidades.NOMBRE_USUARIO, "Shaquille");
-        values.put(Utilidades.CONTRASEÑA_USUARIO, "12345");
-        db.insert(Utilidades.TABLA_USUARIO, null, values);
-
-        values.put(Utilidades.ID_ADMIN, "50");
-        values.put(Utilidades.NOMBRE_ADMIN, "admin");
-        values.put(Utilidades.CONTRASEÑA_ADMIN, "admin");
-        db.insert(Utilidades.TABLA_ADMIN, null, values);
+    //creacion de la tabla de los usuarios
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table usuarios(cedula int primary key,nombre text,apellidos text,usuario text,contraseña text,rol text)");
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        db.execSQL("DROP TABLE IF EXISTS TABLA_USUARIO");
-        db.execSQL("DROP TABLE IF EXISTS TABLA_ADMIN");
-        onCreate(db);
+    //dropear la tabla en caso de que exista
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS usuarios");
+        db.close();
+
     }
+
 }
--- Code/Proyecto_Progra_5_Comedor/app/src/main/java/com/example/proyecto_progra_5_comedor/MainActivity.java
+++ Code/Proyecto_Progra_5_Comedor/app/src/main/java/com/example/proyecto_progra_5_comedor/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.proyecto_progra_5_comedor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,8 +17,9 @@ public class MainActivity extends AppCompatActivity {
     Button btnLogin;
     EditText txtUsuario;
     EditText txtContraseña;
+    String usuario,contraseña;
 
-    ConexionDB db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,31 +28,76 @@ public class MainActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         txtUsuario = findViewById(R.id.txtUsuario);
         txtContraseña = findViewById(R.id.txtContraseña);
+
+        this.agregarusuario();
+
     }
 
     public void Login(View view) {
-        db = new ConexionDB(this);
+        usuario = txtUsuario.getText().toString();
+        contraseña = txtContraseña.getText().toString();
 
-        String usu = txtUsuario.getText().toString();
-        String contra = txtContraseña.getText().toString();
-
-        if(usu.equals(Utilidades.NOMBRE_USUARIO) && contra.equals(Utilidades.CONTRASEÑA_USUARIO)){
-            Intent intent = new Intent(MainActivity.this,MenuEstudiante.class);
-            startActivity(intent);
-            Toast.makeText(this, "Logueo exitoso de Estudiante", Toast.LENGTH_SHORT).show();
-            //acceso a pantalla de usuario lista
+        if (usuario == null || "".equals(contraseña)) {
+            Toast.makeText(this, "Complete los Campos", Toast.LENGTH_SHORT).show();
+        } else {
+            boolean existe = this.existeusuario(usuario, contraseña);
+            if (existe) {
+                boolean tipousuario = this.tipousuario(usuario);
+                if (tipousuario) {
+                    Intent i = new Intent(MainActivity.this, MenuAdmin.class);
+                    startActivity(i);
+                } else {
+                    Intent i = new Intent(MainActivity.this, MenuEstudiante.class);
+                    startActivity(i);
+                }
+            } else {
+                Toast.makeText(this, "Datos Incorrectos", Toast.LENGTH_SHORT).show();
+            }
         }
-        else if (usu.equals(Utilidades.NOMBRE_ADMIN) && contra.equals(Utilidades.CONTRASEÑA_ADMIN)){
-            Intent intent = new Intent(MainActivity.this,MenuAdmin.class);
-            startActivity(intent);
-            Toast.makeText(this, "Logueo exitoso de Admin", Toast.LENGTH_SHORT).show();
-            //acceso a la pantalla de admin lista
-        }
-        else{
-            Toast.makeText(this, "Logueo fallido", Toast.LENGTH_SHORT).show();
-        }
+        txtUsuario.setText("");
+        txtContraseña.setText("");
 
     }
+
+    // se agregan los usuarios dentro de la tabla
+   public void agregarusuario() {
+        ConexionDB conex = new ConexionDB(this,"Proyecto_Progra_5_Comedor",null,1);
+        SQLiteDatabase db = conex.getReadableDatabase();
+
+        db.execSQL("INSERT OR IGNORE INTO  usuarios  (cedula ,nombre ,apellidos  ,usuario ,contraseña ,rol ) Values (123456789,'Shaquille','Lackwood Blanco','Shaco06','1234','Usuario')");
+        db.execSQL("INSERT OR IGNORE INTO usuarios (cedula ,nombre ,apellidos  ,usuario ,contraseña ,rol) Values (987654321,'Steven','Sibaja Araya','Stevensiar','1739','Administrador')");
+        db.close();
+
+    }
+    //si existe el usuario se verifica
+    public boolean existeusuario(String usuario, String contraseña) {
+        ConexionDB conex = new ConexionDB(this,"Proyecto_Progra_5_Comedor",null,1);
+        SQLiteDatabase db =conex.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from usuarios where usuario=? and  contraseña=?", new String[]{usuario, contraseña});
+        if (cursor.getCount() > 0) {
+            return true;
+        } else {
+            db.close();
+        }
+        return false;
+    }
+    //se compara el tipo de usuario a ingresar
+    public boolean tipousuario(String usuario) {
+        ConexionDB conex= new ConexionDB(this,"Proyecto_Progra_5_Comedor",null,1);
+        SQLiteDatabase db =conex.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("select  rol  from usuarios where usuario=?", new String[]{usuario});
+
+        if (cursor.moveToFirst()) {
+            if (cursor.getString(cursor.getColumnIndexOrThrow("rol")).equals("Administrador"))
+                return true;
+        } else {
+            db.close();
+        }
+        return false;
+    }
+
+
     public void cancelar (View pView){
         Toast.makeText(this, "Cancelado", Toast.LENGTH_LONG).show();
         this.limpiar();
